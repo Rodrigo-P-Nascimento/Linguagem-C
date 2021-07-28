890bc4d3e70272bd51d7b5ba4bc18f71465825c3
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-07-28 10:44:32 -0300

    up

--- C LP1/Leitura de arquivos/Teste.c
+++ C LP1/Leitura de arquivos/Teste.c
@@ -16,7 +16,7 @@ int main(void){
     return 0;
 }
 */
-
+/*
 void LeArquivo(int *vet);
 
 int main(void){
@@ -43,8 +43,56 @@ void LeArquivo(int *vet){
     arquivo = fopen("teste02.txt", "w");
 
     for(cont=0; cont < 10; cont++){
-        fprintf(arquivo, "%d\n", vet[cont]+3);
+        fprintf(arquivo, "%d\n", vet[cont]+1);
     }
     
     fclose(arquivo);
+}
+*/
+
+void organizaArquivo(int *vet);
+
+int main(void){
+
+    int vect[11];
+
+    organizaArquivo(&vect[0]);
+
+    printf("Arquivo ordenado com sucesso!\n");
+    system("pause");
+
+    return 0;
+}
+
+void organizaArquivo(int *vet){
+
+    int i, j, aux;
+
+    FILE *ar1;
+
+    ar1 = fopen("testeNumDes.txt", "r");
+
+    for(i=0; i < 10; i++){              //Vamos copiar os valores do arquivo para o array
+        fscanf(ar1, "%d\n", &vet[i]);
+        printf(" %d ", vet[i]);
+    }
+
+    ar1 = fopen("teste03.txt", "w");    //Vamos criar um arquivo, que vai receber os numeros em ordem
+
+    for(i=0;i<10;i++){
+        //bubble sort 
+        for(j=0;j < 10;j++){
+            if(vet[i] > vet[j]){
+
+                aux = vet[i];
+                vet[i] = vet[j];
+                vet[j] = aux;
+            }
+        }
+        printf("Num: %d\n", vet[i]);
+        fprintf(ar1, "%d\n", vet[i]);   //Vamos escrever os valores no arquivo teste03.txt
+    }
+
+    fclose(ar1);
+    
 }
